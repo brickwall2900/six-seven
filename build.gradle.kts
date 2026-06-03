d0d18539ccad5f165319332c4fa7d1ba363229b5
brickwall2900 <brickwall2900@gmail.com>
2026-06-03 21:21:15 +0800

    make publishing possible

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,55 @@
 plugins {
     id("java")
+    id("java-library")
+    id("maven-publish")
+    id("net.thebugmc.gradle.sonatype-central-portal-publisher").version("1.2.4")
 }
 
-group = "io.github.brickwall2900.sixseven"
+group = "io.github.brickwall2900"
 version = "6.7"
+description = "six seven"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+
+    withSourcesJar()
+    withJavadocJar()
+}
+
+signing {
+    useGpgCmd()
+}
+
+centralPortal {
+    pom {
+        name = project.group.toString()
+        description = project.description
+        inceptionYear = "2026"
+        url = "https://codeberg.org/brickwall2900/six-seven"
+
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://mit-license.org/"
+            }
+        }
+
+        developers {
+            developer {
+                id = "brickwall2900"
+                name = "Marsh"
+                email = "brickwall2900@gmail.com"
+            }
+        }
+
+        scm {
+            connection = "scm:https://codeberg.org/brickwall2900/six-seven.git"
+            developerConnection = "scm:git:ssh://git@codeberg.org/brickwall2900/six-seven.git"
+            url = "https://codeberg.org/brickwall2900/six-seven"
+        }
+    }
+}
 
 repositories {
     mavenCentral()
