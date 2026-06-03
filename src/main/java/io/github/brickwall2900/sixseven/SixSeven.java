649fdf5657094fd6b7f0d5c3639c4aa7fe9ed408
brickwall2900 <brickwall2900@gmail.com>
2026-06-03 11:43:29 +0800

    add javadoc

--- src/main/java/io/github/brickwall2900/sixseven/SixSeven.java
+++ src/main/java/io/github/brickwall2900/sixseven/SixSeven.java
@@ -1,8 +1,11 @@
 package io.github.brickwall2900.sixseven;
 
+/// The SixSeven class returns a constant integer value `67`
+/// @see SixSeven#getSixSeven()
 public final class SixSeven {
     private SixSeven() {}
 
+    /// @return the constant integer value `67`
     public static int getSixSeven() {
         return 67;
     }
